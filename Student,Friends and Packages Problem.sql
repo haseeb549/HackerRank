b023fc861c46075a8eaf5526027aec9a4d96e847
Muhammad Haseeb <116158388+haseeb549@users.noreply.github.com>
2023-02-01 14:00:12 +0500

    Update Student,Friends and Packages Problem.sql

--- Student,Friends and Packages Problem.sql
+++ Student,Friends and Packages Problem.sql
@@ -1,9 +1,9 @@
-Question: 
+-- Question: 
 
-You are given three tables: Students, Friends and Packages. Students contains two columns: ID and Name. Friends contains two columns: ID and Friend_ID 
-(ID of the ONLY best friend). Packages contains two columns: ID and Salary (offered salary in $ thousands per month).
+-- You are given three tables: Students, Friends and Packages. Students contains two columns: ID and Name. Friends contains two columns: ID and Friend_ID 
+-- (ID of the ONLY best friend). Packages contains two columns: ID and Salary (offered salary in $ thousands per month).
 
-Solution:
+-- Solution:
 
 SELECT t.Name
 FROM (
