750a8abe84e9f18f6b0c4c50c7c8b27119ea2843
Muhammad Haseeb <116158388+haseeb549@users.noreply.github.com>
2023-02-01 13:43:45 +0500

    Create Julia asked her student part 3.sql

---
+++ Julia asked her student part 3.sql
@@ -0,0 +1,22 @@
+
+QUESTION:
+
+You did such a great job helping Julia with her last coding contest challenge that she wants you to work on this one, too!
+
+The total score of a hacker is the sum of their maximum scores for all of the challenges. Write a query to print the hacker_id, name,
+and total score of the hackers ordered by the descending score. If more than one hacker achieved the same total score, then sort the
+result by ascending hacker_id. Exclude all hackers with a total score of  from your result.
+
+SOLUTION:
+
+select m.hacker_id, h.name, sum(score) as total_score 
+from (
+    select hacker_id, challenge_id, max(score) as score
+from Submissions 
+    group by hacker_id, challenge_id) as m
+join Hackers as h
+on 
+m.hacker_id = h.hacker_id
+group by m.hacker_id, h.name
+having sum(score)> 0
+order by total_score desc, m.hacker_id;
